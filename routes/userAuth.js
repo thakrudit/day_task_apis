3bd5566e8f01876da5054c83ca2b025125cd8e93
thakrudit <thakurudit143@gmail.com>
2024-05-22 12:50:04 +0530

    Create index file for user authontication

--- routes/userAuth.js
+++ routes/userAuth.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const AuthController = require("../controller/userAuth")
+const requireAuthentication = require("../passport/index.js").authenticateUser       
 
 router.post("/sign-up", AuthController.signUp)
 router.post("/log-in", AuthController.logIn)
