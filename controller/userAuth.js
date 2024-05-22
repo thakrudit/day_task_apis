db4ca5d6e013bcd3c67317930b5c0d8dd2bd9a0d
thakrudit <thakurudit143@gmail.com>
2024-05-22 12:41:05 +0530

    Create SignUp and LogIn APIs

--- controller/userAuth.js
+++ controller/userAuth.js
@@ -1,7 +1,89 @@
 const db = require("../models");
+const helper = require("../config/helper")
+const bcrypt = require("bcryptjs")
+const randomstring = require("randomstring")
+const jwt = require("jsonwebtoken")
 
 const User = db.users;
 
 module.exports = {
-    
+    signUp: async (req, res) => {
+        try {
+            const required = {
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password,
+            }
+            const non_required = {}
+            const requestedData = await helper.validateObject(required, non_required);
+
+            const existingUser = await User.findOne({ where: { email: requestedData.email } });
+            if (!!existingUser) {
+                return helper.error(res, "Email Already Exist...");
+            }
+
+            const salt = 10;
+            const hashPassword = await bcrypt.hash(requestedData.password, salt);
+
+            let otp = randomstring.generate({ length: 6, charset: 'numeric' });
+
+            const user = await User.create({
+                name: requestedData.name,
+                profile_url: requestedData.profile_url,
+                email: requestedData.email,
+                password: hashPassword,
+                otp: otp
+            });
+
+            const credentials = { id: user.id, email: user.email };
+            const token = jwt.sign({ data: credentials }, process.env.JWT_SECRET);
+            const body = {
+                token: token,
+                user: user,
+            };
+            return helper.success(res, "Signup Successfull...", body);
+        } catch (err) {
+            return helper.error(res, err)
+        }
+    },
+
+    logIn: async (req, res) => {
+        try {
+            const required = {
+                email: req.body.email,
+                password: req.body.password
+            }
+            const non_required = {}
+            const requestedData = await helper.validateObject(required, non_required);
+
+            const user = await User.findOne({ where: { email: requestedData.email } });
+            if (!user) {
+                return helper.error(res, "Login Email is Incorrect...");
+            }
+
+            const data = await bcrypt.compare(requestedData.password, user.password);
+            if (!data) {
+                return helper.error(res, "Login Password is Incorrect...");
+            }
+            let otp = randomstring.generate({ length: 6, charset: 'numeric' });
+
+            if (user.is_verified === 0) {
+                user.otp = otp
+                user.save();
+                // return helper.success(res, 'OTP Sent')
+            }
+
+            const credentials = { id: user.id, email: user.email };
+            const token = jwt.sign({ data: credentials }, process.env.JWT_SECRET);
+
+            const body = {
+                token: token,
+                user: user,
+            };
+            return helper.success(res, "Login Successfully...", body);
+
+        } catch (err) {
+            return helper.error(res, err)
+        }
+    },
 }
--- routes/userAuth.js
+++ routes/userAuth.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const AuthController = require("../controller/userAuth")
 
-
+router.post("/sign-up", AuthController.signUp)
+router.post("/log-in", AuthController.logIn)
 
 module.exports = router
